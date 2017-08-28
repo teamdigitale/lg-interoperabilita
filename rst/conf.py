60a11cba765cf1374577e173231c0df429d2681c
Simone Piunno <simonep@gmail.com>
2017-08-28 18:36:20 +0200

    Corretto il titolo del progetto

--- rst/conf.py
+++ rst/conf.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # -- PROJECT Variables ----------------------------------------------------
-settings_project_name = "Codice dell'amministrazione digitale"
+settings_project_name = "Linee guida di interoperabilità"
 settings_copyright_year = '2017'
 settings_copyright_name = ''
 settings_doc_version = 'version: latest'
